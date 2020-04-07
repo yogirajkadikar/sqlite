d8e9f8572dbce3529e38c2fddd47e4999392f192
Yogiraj <yogiraj.kadikar@gmail.com>
2020-04-07 20:16:21 +0530

    check db

--- contacts2.py
+++ contacts2.py
@@ -6,6 +6,12 @@ update_sql = "UPDATE contacts SET email = 'update@rmail.com' WHERE contacts.phon
 update_cursor =db.cursor()
 update_cursor.execute(update_sql)
 print("{} rows updated".format(update_cursor.rowcount))
+
+print()
+print("Are connections the same: {}".format(update_cursor.connection == db))
+print()
+
+update_cursor.connection.commit()
 update_cursor.close()
 for name, phone, email in db.execute("SELECT * FROM contacts"):
     print(name)
